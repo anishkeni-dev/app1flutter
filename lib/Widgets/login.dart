3d9191718e1e0f6be4aef6837312b888defbd92e
anish.keni <anish.keni@srijan.net>
2022-09-19 16:29:29 +0530

    mvvm app1
    Signed-off-by: anish.keni <anish.keni@srijan.net>

--- lib/Widgets/login.dart
+++ lib/Widgets/login.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:app1/UI/home_view.dart';
+import 'package:another_flushbar/flushbar.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/safe_area_values.dart';
+import 'package:top_snackbar_flutter/tap_bounce_container.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
 Future <dynamic> readJson(uname,pswd,context) async {
   List _user = [];
@@ -11,8 +16,13 @@ Future <dynamic> readJson(uname,pswd,context) async {
   final data = jsonDecode(response);
   _user = data['User'];
   if(_user[0]['username'] != uname && _user[0]['password'] != pswd) {
-    return ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content:Text('User not found! Sign up now')));
+    return showTopSnackBar(
+      context,
+      CustomSnackBar.error(
+        message: "User not found! Please Sign-up"
+      ),
+    );
+
   }
 
   else {
@@ -27,11 +37,21 @@ Future <dynamic> readJson(uname,pswd,context) async {
 
      else{
           if(_user[0]['username'] != uname) {
-            return ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                content: Text('Incorrect Username! Please try again')));
+            return showTopSnackBar(
+              context,
+              CustomSnackBar.error(
+                  message: 'Incorrect Username! Please try again'
+              ),
+            );
           }
-          else{return ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text('Incorrect Password! Please try again')));}
+          else{
+            return showTopSnackBar(
+              context,
+              CustomSnackBar.error(
+                  message: 'Incorrect Password! Please try again'
+              ),
+            );
+          }
       }
 }
 }
